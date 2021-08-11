ac1c99c1334a7c91f232ff22c226ae3361623d07
eoiles <stary-@live.com>
2021-08-11 15:05:04 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,12 @@
-function sub(){
-    if (arguments.length==0){
-        return field("link")[0]
-    }
-    else if (arguments.length==1){
-        field("link").forEach(function(element){
-            if(element.field("name")==arguments[0]){
-                return element
-            }
-        })
-    }
-    else if (arguments.length==2){
-        arguments[0].field("link").forEach(function(element){
-            if(element.field("name")==arguments[1]){
-                return element
-            }
-        })
-        
-    }
+function sub() {
+  if (arguments.length == 0) {
+    return field("link")[0]
+  }
+  else if (arguments.length == 1) {
+    field("link").forEach(function(element) {
+      if (element.field("name") == arguments[0]) {
+        return element
+      }
+    })
+  }
 }
