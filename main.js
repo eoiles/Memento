81f9f51731dba57e69cfc15c259b5e3f41dac40e
eoiles <stary-@live.com>
2021-08-11 15:17:37 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-function sub() {
-  if (arguments.length == 0) {
-    return field("link")[0]
-  }
-  else if (arguments.length == 1) {
-    field("link").forEach(function(element) {
-      if (element.field("name") == arguments[0]) {
-        return element
-      }
-    })
-  }
+function sub(){
+    if (arguments.length==0){
+        return this.field("link")[0]
+    }
+    else if (arguments.length==1){
+        return this.field("link").find(x=>x.field("name")==arguments[0])
+        })
+    }
+    else if (arguments.length==2){
+        return arguments[0].field("link").find(x=>x.field("name")==arguments[1])
+            }
+        })
+        
+    }
 }
