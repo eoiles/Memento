6d4b2d6d3dbc3e1da447165ebbc0c71efa37f8f7
eoiles <stary-@live.com>
2021-08-11 15:21:15 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,14 +2,4 @@ function sub(){
     if (arguments.length==0){
         return field("link")[0]
     }
-    else if (arguments.length==1){
-        return field("link").find(x=>x.field("name")==arguments[0])
-        })
-    }
-    else if (arguments.length==2){
-        return arguments[0].field("link").find(x=>x.field("name")==arguments[1])
-            }
-        })
-        
-    }
 }
